5743a6101debf5a357786a99f98efe60d501eaee
london804 <alexechaparro@gmail.com>
2018-10-02 22:59:59 -0400

    working on form validation

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { AbstractControl, FormGroup, FormControl, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -22,9 +31,12 @@ export class CustomerComponent implements OnInit {
 
     // this is the form model that tracks the value and state
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: {value: 'n/a', disabled: true},
+      phone: '',
+      notification: 'email',
+      rating: ['' , ratingRange(1,5)],
       sendCatalog: true
     })
 
@@ -49,5 +61,15 @@ export class CustomerComponent implements OnInit {
       email: 'jack@torchwood.com',
       sendCatalog: false
     })
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
